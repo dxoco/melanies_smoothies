2061980863e9b245c7fa61d7f0a926d7a4e440a9
dxoco <67029492+dxoco@users.noreply.github.com>
2024-07-28 19:33:22 -0600

    Update streamlit_app.py
    fruityvice API integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize your smoothie! :cup_with_straw:")
@@ -24,6 +25,11 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
+# api integration
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+
 if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
@@ -41,3 +47,4 @@ if ingredients_list:
         if ingredients_string:
             session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
+
