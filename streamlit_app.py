3fc3d2b253ae8ad936920937a7efdb1c41ba9e2b
dxoco <67029492+dxoco@users.noreply.github.com>
2024-07-28 20:29:05 -0600

    Update streamlit_app.py
    add LOC search using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + " "
+        # search using search_on column in pandas df
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit,' is ', search_on, '.')
         st.subheader( fruit + ' Nutrition Information' )
         # get API data
         fruityvice_response = requests.get( "https://fruityvice.com/api/fruit/" + fruit )
