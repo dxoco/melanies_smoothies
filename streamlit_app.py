c0b82be193a1fafe76a714d6da6fd4144bd37281
dxoco <67029492+dxoco@users.noreply.github.com>
2024-07-28 19:50:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
         ingredients_string += fruit + " "
         st.subheader( fruit + 'Nutrition Information' )
         # get API data
-        fruityvice_response = requests.get( "https://fruityvice.com/api/fruit/" + fruit_chosen )
+        fruityvice_response = requests.get( "https://fruityvice.com/api/fruit/" + fruit )
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
         
     st.write(ingredients_string)
